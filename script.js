3ebe059284075b058bc723803a5ff00c791f493b
Vallentini <vallentinigabrielli.silva@gmail.com>
2023-12-07 09:26:17 -0300

    acabei explicação nos comentários, já que não deu para apresentar

--- script.js
+++ script.js
@@ -1,74 +1,76 @@
-var input = document.querySelector("#display");
+/**Adicionar as entradas: números, operadores e limpar o histórico*/
+
+var entradaNum = document.querySelector("#display");
 var operador = "";
 var parar = false;
-
-function limpar() {
-  input.value = "";
+/*função para apagar o histórico: se as entradas forem vazias ou seja, não for add um novo número para uma nova operação, e o usúario clicar no button "AC" onde o id="hystory", o calculo feito e o histórico da conta será apagado*/
+function apagarhistorico() {
+  entradaNum.value = "";
   document.querySelector("#history").innerHTML = "";
-  operador = ""
+  operador = "";
 }
-
-function add_numero(string) {
+/*função para add o número como uma string */
+function numero(string) {
   if (!parar) {
-    if (string === '.' && input.value.indexOf(".") === -1 || string !== '.') {
-      input.value = input.value + string;
-      history.value = input.value;
+    if (string === '.' && entradaNum.value.indexOf(".") === -1 || string !== '.') {
+      entradaNum.value = entradaNum.value + string;
+      history.value = entradaNum.value;
     }
   } else {
-    if (string === '.' && input.value.indexOf(".") === -1 || string !== '.') {
-      input.value = "";
+    if (string === '.' && entradaNum.value.indexOf(".") === -1 || string !== '.') {
+      entradaNum.value = "";
       parar = false;
       document.querySelector("#history").innerHTML = "";
       operador = "";
 
-      input.value = input.value + string;
+      entradaNum.value = entradaNum.value + string;
     }
   }
 }
-
-function add_operador(op) {
-  if (input.value == "") {
-    input.value = "0";
+/*Funçaõ add o operador ao lado da primeira string, não o deixando em cima do número. */
+function operador(opera) {
+  if (entradaNum.value == "") {
+    entradaNum.value = "0";
   }
 
   if (operador == "") {
-    operador = op;
-    input.value = input.value + op;
+    operador = opera;
+    entradaNum.value = entradaNum.value + opera;
   } else {
-    total()
-    input.value = input.value + op;
-    operador = op;
+    totaldaconta()
+    entradaNum.value = entradaNum.value + opera;
+    operador = opera;
     parar=false;
   }
 }
-
-function total() {
+/*função para resolver a operção, no primeiro if é para igualar a entrada do usúario, no segundo if é caso for diferente de parar e a string for igual a nulo a string será == 0, então temos o histórico que será apresentado. Por fim o switch case onde resolvera as operações*/
+function totaldaconta() {
   if (!parar) {
-    numeros = input.value.split(operador);
+    numeros = entradaNum.value.split(operador);
 
     if (numeros[1] == "") {
-      input.value = input.value + '0';
+      entradaNum.value = entradaNum.value + '0';
       numeros[1] = 0;
 
     }
 
-    document.querySelector("#history").innerHTML = input.value;
+    document.querySelector("#history").innerHTML = entradaNum.value;
 
     switch (operador) {
       case "+":
-        input.value = Number(numeros[0]) + Number(numeros[1]);
+        entradaNum.value = Number(numeros[0]) + Number(numeros[1]);
         break;
 
       case "-":
-        input.value = numeros[0] - numeros[1];
+        entradaNum.value = numeros[0] - numeros[1];
         break;
 
       case "*":
-        input.value = numeros[0] * numeros[1];
+        entradaNum.value = numeros[0] * numeros[1];
         break;
 
       case "/":
-        input.value = numeros[0] / numeros[1];
+        entradaNum.value = numeros[0] / numeros[1];
         break;
     }
 
